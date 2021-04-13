ecf44b26851a0729e702424c6eef4e699af3de66
Ivy-Walobwa <ivywwalobwa@gmail.com>
2021-04-13 11:27:26 +0300

    sembast CRUD

--- lib/data/sembast_db.dart
+++ lib/data/sembast_db.dart
@@ -5,7 +5,7 @@ import 'package:path_provider/path_provider.dart';
 
 import '../models/password.dart';
 
-class SembastDb{
+class SembastDb {
   // databaseFactoryIo allows to open sembast database
   // sembast database is single file
   DatabaseFactory databaseFactory = databaseFactoryIo;
@@ -18,19 +18,18 @@ class SembastDb{
 
   SembastDb._internal();
 
-  factory SembastDb(){
+  factory SembastDb() {
     return _singletone;
   }
 
-
-  Future<Database> init()async{
-    if(_db == null){
+  Future<Database> init() async {
+    if (_db == null) {
       _db = await _openDb();
     }
     return _db;
   }
 
-  Future _openDb()async{
+  Future _openDb() async {
     // retrieve apps doc dir to save sembast db
     final docsDir = await getApplicationDocumentsDirectory();
     final dbPath = join(docsDir.path, 'pass.db');
@@ -40,4 +39,33 @@ class SembastDb{
     return db;
   }
 
+  Future<int> addPassword(Password pwd) async {
+    int id = await store.add(_db, pwd.toJson());
+    return id;
+  }
+
+  Future getPasswords() async {
+    await init();
+    final finder = Finder(sortOrders: [SortOrder('name')]);
+    final snapshot = await store.find(_db, finder: finder);
+    return snapshot.map((item) {
+      final pwd = Password.fromJson(item.value);
+      pwd.id = item.key;
+      return pwd;
+    }).toList();
+  }
+
+  Future updatePassword(Password pwd) async {
+    final finder = Finder(filter: Filter.byKey(pwd.id));
+    await store.update(_db, pwd.toJson(), finder: finder);
+  }
+
+  Future deletePassword(Password pwd) async {
+    final finder = Finder(filter: Filter.byKey(pwd.id));
+    await store.delete(_db, finder: finder);
+  }
+
+  Future deleteAll() async {
+    await store.delete(_db);
+  }
 }
