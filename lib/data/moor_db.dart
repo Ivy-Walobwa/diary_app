04e14440b2f146575fad73e2fdf7b430d00af6cd
Ivy-Walobwa <ivywalobwa@gmail.com>
2021-04-24 18:34:56 +0300

    CRUD with Moor

--- lib/data/moor_db.dart
+++ lib/data/moor_db.dart
@@ -2,19 +2,32 @@ import 'package:moor_flutter/moor_flutter.dart';
 
 part 'moor_db.g.dart';
 
-class BlogPost extends Table{
+class BlogPost extends Table {
   // also written as :
   // IntColumn get id => integer().autoIncrement().call();
   IntColumn get id => integer().autoIncrement()();
+
   TextColumn get name => text().withLength(min: 1, max: 80)();
+
   TextColumn get content => text().nullable()();
+
   DateTimeColumn get date => dateTime().nullable()();
 }
 
 @UseMoor(tables: [BlogPost])
-class MoorDb extends _$MoorDb{
-  MoorDb():super(FlutterQueryExecutor.inDatabaseFolder(path: 'blog.db'));
+class MoorDb extends _$MoorDb {
+  MoorDb() : super(FlutterQueryExecutor.inDatabaseFolder(path: 'blog.db'));
+
   @override
   int get schemaVersion => 1;
 
+  Future<List<BlogPostData>> getPosts() => (select(blogPost)
+        ..orderBy([
+          (post) => OrderingTerm(expression: post.date, mode: OrderingMode.desc)
+        ]))
+      .get();
+
+  Future<int> newPost(BlogPostData post)=> into(blogPost).insert(post);
+  Future<bool> updatePost(BlogPostData post)=> update(blogPost).replace(post);
+  Future<int> deletePost(BlogPostData post)=> delete(blogPost).delete(post);
 }
